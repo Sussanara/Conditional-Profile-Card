dad42d0977f71b6dc5bf3798a3b12943958f3fe9
Sussanara <sussanaracely@gmail.com>
2022-06-23 20:43:15 +0000

    Profile Card

--- src/js/app.js
+++ src/js/app.js
@@ -63,7 +63,7 @@ function render(variables = {}) {
           <img src="${variables.avatarURL}" class="photo" />
           <h1>${name} ${lastname}</h1>
           <h2>Web Developer</h2>
-          <h3>${Santiago}, ${Chile}</h3>
+          <h3>${variables.city}, ${variables.country}</h3>
           <ul class="${socialMediaPosition}">
             <li><a href="https://twitter.com/${variables.twitter}"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/${variables.github}"><i class="fab fa-github"></i></a></li>
